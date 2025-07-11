aaf92bb0bf5d55e71f985bd1167f051ea0237104
Edwin Boon <hello@edwinboon.dev>
2025-07-11 11:57:03 +0200

    its now possible to list the content of a book

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(book_path: str) -> str:
+    with open(book_path) as f:
+        return f.read()
+
+
+def main():
+    BOOK_PATH = "./books/frankenstein.txt"
+    book_text = get_book_text(BOOK_PATH)
+    print(book_text)
+
+
+main()

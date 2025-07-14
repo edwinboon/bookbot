dd62adb532ac40f72f6f910101c43a5977d02f4b
Edwin Boon <hello@edwinboon.dev>
2025-07-14 11:11:18 +0200

    Add more functions and we can now print a report

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from stats import (
+    get_character_count,
+    get_number_of_words,
+    get_sorted_character_count,
+    print_report,
+)
+
+
 def get_book_text(book_path: str) -> str:
     with open(book_path) as f:
         return f.read()
@@ -6,7 +14,11 @@ def get_book_text(book_path: str) -> str:
 def main():
     BOOK_PATH = "./books/frankenstein.txt"
     book_text = get_book_text(BOOK_PATH)
-    print(book_text)
+    number_of_words = get_number_of_words(book_text)
+    character_count = get_character_count(book_text)
+    sorted_character_count = get_sorted_character_count(character_count)
+
+    print_report(BOOK_PATH, number_of_words, sorted_character_count)
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,36 @@
+def get_number_of_words(text: str) -> int:
+    return len(text.split())
+
+
+def get_character_count(text: str) -> dict:
+    counts_dict = {}
+    for char in text.lower():
+        if char in counts_dict:
+            counts_dict[char] += 1
+        else:
+            counts_dict[char] = 1
+    return counts_dict
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def get_sorted_character_count(character_count: dict) -> list:
+    sorted_count = []
+    for key, value in character_count.items():
+        if key.isalpha():
+            sorted_count.append({"char": key, "num": value})
+
+    sorted_count.sort(reverse=True, key=sort_on)
+    return sorted_count
+
+
+def print_report(book_path: str, number_of_words: int, character_count: dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("---------- Character Count -------")
+    for item in character_count:
+        print(f"{item['char']}: {item['num']}")
